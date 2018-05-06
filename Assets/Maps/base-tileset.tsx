a4f9469a69cb587ee0d202dad2b44b7e60fed0cb
nqlu <nqlu@deakin.edu.au>
2018-05-06 23:52:00 +1000

    update stage

--- Assets/Maps/base-tileset.tsx
+++ Assets/Maps/base-tileset.tsx
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="base-tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="../../../../Desktop/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <image source="../../../../roguelikeSheet_transparent.png" width="968" height="526"/>
  <terraintypes>
+  <terrain name="Grass" tile="589"/>
   <terrain name="dirt" tile="0"/>
  </terraintypes>
  <tile id="2">
@@ -49,24 +50,46 @@
    <object id="1" x="-0.0909091" y="-0.0909091" width="16.0909" height="16"/>
   </objectgroup>
  </tile>
- <tile id="518" terrain="0,0,0,"/>
- <tile id="519" terrain="0,0,,0"/>
- <tile id="520" terrain=",,,0"/>
- <tile id="521" terrain=",,0,0"/>
- <tile id="522" terrain=",,0,"/>
+ <tile id="342" terrain="0,0,0,"/>
+ <tile id="343" terrain="0,0,,0"/>
+ <tile id="344" terrain=",,,0"/>
+ <tile id="345" terrain=",,0,0"/>
+ <tile id="346" terrain=",,0,"/>
+ <tile id="399" terrain="0,,0,0"/>
+ <tile id="400" terrain=",0,0,0"/>
+ <tile id="401" terrain=",0,,0"/>
+ <tile id="403" terrain="0,,0,"/>
+ <tile id="458" terrain=",0,,"/>
+ <tile id="459" terrain="0,0,,"/>
+ <tile id="460" terrain="0,,,"/>
+ <tile id="518" terrain="1,1,1,"/>
+ <tile id="519" terrain="1,1,,1"/>
+ <tile id="520" terrain=",,,1"/>
+ <tile id="521" terrain=",,1,1"/>
+ <tile id="522" terrain=",,1,"/>
  <tile id="526">
   <objectgroup draworder="index">
    <object id="1" x="0" y="-0.181818" width="16.2727" height="16.2727"/>
   </objectgroup>
  </tile>
- <tile id="575" terrain="0,,0,0"/>
- <tile id="576" terrain=",0,0,0"/>
- <tile id="577" terrain=",0,,0"/>
- <tile id="578" terrain="0,0,0,0"/>
- <tile id="579" terrain="0,,0,"/>
- <tile id="634" terrain=",0,,"/>
- <tile id="635" terrain="0,0,,"/>
- <tile id="636" terrain="0,,,"/>
+ <tile id="575" terrain="1,,1,1"/>
+ <tile id="576" terrain=",1,1,1"/>
+ <tile id="577" terrain=",1,,1"/>
+ <tile id="578" terrain="1,1,1,1"/>
+ <tile id="579" terrain="1,,1,"/>
+ <tile id="616">
+  <objectgroup draworder="index">
+   <object id="1" x="-2" y="-0.666667" width="20" height="16.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="617">
+  <objectgroup draworder="index">
+   <object id="1" x="-1.33333" y="-0.666667" width="17.3333" height="16.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="634" terrain=",1,,"/>
+ <tile id="635" terrain="1,1,,"/>
+ <tile id="636" terrain="1,,,"/>
  <tile id="643">
   <objectgroup draworder="index">
    <object id="1" x="-0.0909091" y="-0.272727" width="16.1818" height="16.3636"/>
@@ -75,6 +98,16 @@
  <tile id="645">
   <objectgroup draworder="index">
    <object id="1" x="-0.363636" y="0" width="16.6364" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="673">
+  <objectgroup draworder="index">
+   <object id="1" x="0.666667" y="-0.666667" width="15.3333" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="674">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.666667" width="16" height="15.3333"/>
   </objectgroup>
  </tile>
  <tile id="1217">
@@ -106,6 +139,11 @@
   <objectgroup draworder="index">
    <object id="1" x="-57" y="-114"/>
    <object id="2" x="0" y="0.363636" width="16" height="15.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="1307">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.666667" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="1330">
