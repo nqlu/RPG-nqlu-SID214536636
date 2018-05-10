e6b45d21e7c7abf71016a865e584a571b25f1627
nqlu <nqlu@deakin.edu.au>
2018-05-10 14:44:20 +1000

    Add files via upload

---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Invoke("SpawnNext", 2f);
+    }
+    void SpawnNext()
+    {
+        GameObject FallingEggs = Instantiate(gameObject);
+        FallingEggs.transform.position = new Vector3(0, -1 + transform.localScale.y / 2, 0);
+    }
+}
